b3335c653bc91a41dfef9737671763d19b75cad5
Greg Reed <greg@goraff.com>
2015-08-18 02:04:53 -0700

    order updater

--- htdocs/orderupdate/index.php
+++ htdocs/orderupdate/index.php
@@ -2,17 +2,22 @@
 require 'vendor/autoload.php';
 require_once( 'lib/woocommerce-api.php' );
 
-$app = new Slim();
-$app->post('/printio-update', 'addNote'); 
-$app->run();
-
-
-function addNote() {
-    $request = Slim::getInstance()->request();
-    $note = json_decode($request->getBody());
-    echo json_encode($note);
+$app = new \Slim\Slim();
+$app->post('/printio-update', function () use ($app){
+    $json = $app->request->getBody();
+    $data = json_decode($json, true);
+    
+    for ($i =0; $i <= count($data[Items]); $i++) {
+    
+        if ($data[Items][$i][TrackingNumber]){
+    print_r ('Tracking Number: '.$data[Items][$i][TrackingNumber].' Tracking URL: '.$data[Items][$i][TrackingUrl]);
+        }
+    
 }
-
+    
+    
+}); 
+$app->run();
 ?>
 
 
