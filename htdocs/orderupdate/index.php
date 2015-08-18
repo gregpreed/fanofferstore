dff2ecebcdf98e0c05bde767c25dfdac6cfb0ef8
Greg Reed <greg@goraff.com>
2015-08-18 00:37:38 -0700

    update order

--- htdocs/orderupdate/index.php
+++ htdocs/orderupdate/index.php
@@ -2,8 +2,8 @@
 require 'vendor/autoload.php';
 require_once( 'lib/woocommerce-api.php' );
 
-$app = new \Slim\App();
-$app->post('/printio-update', function ($request, $response, $args) {
+$app = new \Slim\Slim();
+$app->post('/printio-update', function () {
     
     
     $paramValue = $app->request->post('Hello');
