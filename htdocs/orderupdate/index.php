30fea97d7e07425c93c3d69e456708a75bedf883
Greg Reed <greg@goraff.com>
2015-08-17 20:26:51 -0700

    update

--- htdocs/orderupdate/index.php
+++ htdocs/orderupdate/index.php
@@ -1,11 +1,8 @@
 <?php
 require 'vendor/autoload.php';
-require_once( 'lib/woocommerce-api.php' );
-
 $app = new \Slim\Slim();
 $app->get('/hello/:name', function ($name) {
     echo "Hello, " . $name;
 });
 $app->run();
 
-?>
