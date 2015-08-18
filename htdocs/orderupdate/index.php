b9aa2937d4724c7ddda8ff308f43d059b6f7a56c
Greg Reed <greg@goraff.com>
2015-08-18 02:27:18 -0700

    syntax fix

--- htdocs/orderupdate/index.php
+++ htdocs/orderupdate/index.php
@@ -18,18 +18,14 @@ $app->post('/printio-update', function () use ($app){
 
 	$client = new WC_API_Client( 'https://fanoffers.com', 'ck_b2b780b5f7a4cd3cd5484cbda1149a26', 'cs_b7b7765ca1b683318809e6b4b5bb804b', $options );
     
-     
+         
     for ($i =0; $i <= count($data['Items']); $i++) {
     if( isset( $data['Items'][$i]['TrackingNumber']) ){
         print_r( $client->order_notes->create( 1201, array( 'note' => 'Tracking Number: '.$data['Items'][$i]['TrackingNumber'].' Tracking URL: '.$data['Items'][$i]['TrackingUrl'] ) ) );
         }
-    
-        
+}
 
-    
-}
-}
-    
+
 }); 
 $app->run();
 
